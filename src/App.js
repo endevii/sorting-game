69b937f15a65b9922ef714677efd06cb0a3b6594
Joshua Gorman <itssnughere@gmail.com>
2023-11-15 19:29:12 -0500

    Just messing around a bit (dndkit)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+import TestDrag from './components/TestDrag';
+import GameBoard from './components/GameBoard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TestDrag />
+      <div className="main_game">
+        <GameBoard boardSize={8} />
+      </div>
     </div>
   );
 }
